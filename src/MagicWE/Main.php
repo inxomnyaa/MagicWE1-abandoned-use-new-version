311f17477c3a7dd13ddebf70910433e57bcd295b
thebigsmileXD <thebigsmilexd@gmail.com>
2016-04-22 11:23:44 +0200

    Fixed undo, added redo, "added" flip, schematic export blocks fix

--- src/MagicWE/Main.php
+++ src/MagicWE/Main.php
@@ -27,10 +27,12 @@ use pocketmine\level\Position;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\item\Item;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\level\generator\normal\biome\PlainBiome;
+use pocketmine\level\Location;
 
 class Main extends PluginBase implements Listener{
 	public $areas;
-	private $pos1 = [], $pos2 = [], $copy = [], $copypos = [], $undo = [], $undometa = [], $undopos = [], $wand = [], $schematics = [];
+	private $pos1 = [], $pos2 = [], $copy = [], $copypos = [], $undo = [], $redo = [], $wand = [], $schematics = [];
 	private static $MAX_BUILD_HEIGHT = 128;
 
 	public function onLoad(){
@@ -125,12 +127,39 @@ class Main extends PluginBase implements Listener{
 				case "/undo":
 					{
 						if(!$sender->hasPermission("magicwe.command.undo")) return;
-						if(isset($this->undopos[$sender->getName()])){
+						if(!empty($this->undo[$sender->getName()])){
 							$this->undo($sender);
 							return true;
 						}
 						else{
 							$sender->sendMessage(TextFormat::RED . "[MagicWE] Nothing to undo");
+						}
+						break;
+					}
+				
+				case "/redo":
+					{
+						if(!$sender->hasPermission("magicwe.command.redo")) return;
+						if(!empty($this->redo[$sender->getName()])){
+							$this->redo($sender);
+							return true;
+						}
+						else{
+							$sender->sendMessage(TextFormat::RED . "[MagicWE] Nothing to redo");
+						}
+						break;
+					}
+				
+				case "/flip":
+					{
+						if(!$sender->hasPermission("magicwe.command.flip")) return;
+						if(!empty($this->copy[$sender->getName()]) && isset($args[0])){
+							if(!in_array($args[0], array("x", "y", "z"))) return false;
+							$this->flip($sender, $args[0]);
+							return true;
+						}
+						else{
+							$sender->sendMessage(TextFormat::RED . "[MagicWE] Nothing to flip, use //copy first");
 						}
 						break;
 					}
@@ -169,7 +198,7 @@ class Main extends PluginBase implements Listener{
 						}
 						elseif($args[0] === "paste"){
 							if(isset($this->schematics[$args[1]]) && $this->schematics[$args[1]] instanceof SchematicLoader){
-								$success = $this->pasteSchematic($sender->getLevel(), $sender, $this->schematics[$args[1]]);
+								$success = $this->pasteSchematic($sender, $sender->getLevel(), $sender->getPosition(), $this->schematics[$args[1]]);
 								if($success){
 									$sender->sendMessage(TextFormat::GREEN . "[MagicWE] Schematic $args[1] sucessfully pasted");
 									return true;
@@ -203,18 +232,45 @@ class Main extends PluginBase implements Listener{
 		return false;
 	}
 
+	public function wandPos1(BlockBreakEvent $event){
+		$sender = $event->getPlayer();
+		$block = $event->getBlock();
+		if($sender->hasPermission("magicwe.command.wand") && $sender->getInventory()->getItemInHand()->getId() === Item::WOODEN_AXE && $this->wand[$sender->getName()] === 1){
+			if($block->y > self::$MAX_BUILD_HEIGHT || $block->y < 0) $sender->sendMessage(TextFormat::GOLD . "[MagicWE] Warning: You are above y:" . self::$MAX_BUILD_HEIGHT . " or below y:0");
+			$this->pos1[$sender->getName()] = $block;
+			$sender->sendMessage(TextFormat::GREEN . "[MagicWE] Position 1 set as x:" . $block->x . " y:" . $block->y . " z:" . $block->z);
+			$event->setCancelled();
+		}
+	}
+
+	public function wandPos2(PlayerInteractEvent $event){
+		if($event->getAction() !== PlayerInteractEvent::RIGHT_CLICK_BLOCK) return;
+		$sender = $event->getPlayer();
+		$block = $event->getBlock();
+		if($sender->hasPermission("magicwe.command.wand") && $sender->getInventory()->getItemInHand()->getId() === Item::WOODEN_AXE && $this->wand[$sender->getName()] === 1){
+			if($block->y > self::$MAX_BUILD_HEIGHT || $block->y < 0) $sender->sendMessage(TextFormat::GOLD . "[MagicWE] Warning: You are above y:" . self::$MAX_BUILD_HEIGHT . " or below y:0");
+			$this->pos2[$sender->getName()] = $block;
+			$sender->sendMessage(TextFormat::GREEN . "[MagicWE] Position 2 set as x:" . $block->x . " y:" . $block->y . " z:" . $block->z);
+			$event->setCancelled();
+		}
+	}
+
 	public function fill(Player $player, $blockarg){
 		if(empty($blockarg) && $blockarg !== "0") return false;
 		$level = $player->getLevel();
 		$blocks = explode(",", $blockarg);
-		$this->undo[$player->getName()] = [];
-		$this->undopos[$player->getName()] = $this->pos1[$player->getName()];
-		for($x = min($this->pos1[$player->getName()]->x, $this->pos2[$player->getName()]->x); $x <= max($this->pos1[$player->getName()]->x, $this->pos2[$player->getName()]->x); $x++){
-			for($y = min($this->pos1[$player->getName()]->y, $this->pos2[$player->getName()]->y); $y <= max($this->pos1[$player->getName()]->y, $this->pos2[$player->getName()]->y); $y++){
+		$pos1 = $this->pos1[$player->getName()];
+		$pos2 = $this->pos2[$player->getName()];
+		$pos = new Vector3(min($pos1->x, $pos2->x), min($pos1->y, $pos2->y), min($pos1->z, $pos2->z));
+		if(!isset($this->undo[$player->getName()])) $this->undo[$player->getName()] = [];
+		$undoindex = count(array_keys($this->undo[$player->getName()]));
+		$this->undo[$player->getName()][$undoindex] = [];
+		for($x = $pos->x; $x <= max($pos1->x, $pos2->x); $x++){
+			for($y = $pos->y; $y <= max($pos1->y, $pos2->y); $y++){
 				if($y > self::$MAX_BUILD_HEIGHT || $y < 0) continue;
-				for($z = min($this->pos1[$player->getName()]->z, $this->pos2[$player->getName()]->z); $z <= max($this->pos1[$player->getName()]->z, $this->pos2[$player->getName()]->z); $z++){
+				for($z = $pos->z; $z <= max($pos1->z, $pos2->z); $z++){
 					if(!$level->isChunkLoaded($x, $z)) $level->loadChunk($x, $z, true);
-					$this->undo[$player->getName()][$x][$y][$z] = $level->getBlock($this->pos1[$player->getName()]->add($x, $y, $z));
+					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock(new Vector3($x, $y, $z)));
 					$blockstring = $blocks[array_rand($blocks, 1)];
 					$block = explode(":", $blockstring)[0];
 					$level->setBlockIdAt($x, $y, $z, $block);
@@ -231,14 +287,18 @@ class Main extends PluginBase implements Listener{
 		$level = $player->getLevel();
 		$blocks1 = explode(",", $blockarg1);
 		$blocks2 = explode(",", $blockarg2);
-		$this->undo[$player->getName()] = [];
-		$this->undopos[$player->getName()] = $this->pos1[$player->getName()];
-		for($x = min($this->pos1[$player->getName()]->x, $this->pos2[$player->getName()]->x); $x <= max($this->pos1[$player->getName()]->x, $this->pos2[$player->getName()]->x); $x++){
-			for($y = min($this->pos1[$player->getName()]->y, $this->pos2[$player->getName()]->y); $y <= max($this->pos1[$player->getName()]->y, $this->pos2[$player->getName()]->y); $y++){
+		$pos1 = $this->pos1[$player->getName()];
+		$pos2 = $this->pos2[$player->getName()];
+		$pos = new Vector3(min($pos1->x, $pos2->x), min($pos1->y, $pos2->y), min($pos1->z, $pos2->z));
+		if(!isset($this->undo[$player->getName()])) $this->undo[$player->getName()] = [];
+		$undoindex = count(array_keys($this->undo[$player->getName()]));
+		$this->undo[$player->getName()][$undoindex] = [];
+		for($x = $pos->x; $x <= max($pos1->x, $pos2->x); $x++){
+			for($y = $pos->y; $y <= max($pos1->y, $pos2->y); $y++){
 				if($y > self::$MAX_BUILD_HEIGHT || $y < 0) continue;
-				for($z = min($this->pos1[$player->getName()]->z, $this->pos2[$player->getName()]->z); $z <= max($this->pos1[$player->getName()]->z, $this->pos2[$player->getName()]->z); $z++){
+				for($z = $pos->z; $z <= max($pos1->z, $pos2->z); $z++){
 					if(!$level->isChunkLoaded($x, $z)) $level->loadChunk($x, $z, true);
-					$this->undo[$player->getName()][$x][$y][$z] = $level->getBlock($this->pos1[$player->getName()]->add($x, $y, $z));
+					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock(new Vector3($x, $y, $z)));
 					foreach($blocks1 as $blockstring1){
 						$block1 = explode(":", $blockstring1)[0];
 						$meta1 = (isset(explode(":", $blockstring1)[1])?explode(":", $blockstring1)[1]:false);
@@ -277,14 +337,15 @@ class Main extends PluginBase implements Listener{
 	public function paste(Player $player){
 		$level = $player->getLevel();
 		$pos = $player->getPosition()->add($this->copypos[$player->getName()]);
-		$this->undo[$player->getName()] = [];
-		$this->undopos[$player->getName()] = $pos;
+		if(!isset($this->undo[$player->getName()])) $this->undo[$player->getName()] = [];
+		$undoindex = count(array_keys($this->undo[$player->getName()]));
+		$this->undo[$player->getName()][$undoindex] = [];
 		for($x = 0; $x < count(array_keys($this->copy[$player->getName()])); $x++){
 			for($y = 0; $y < count(array_keys($this->copy[$player->getName()][$x])); $y++){
 				if($y > self::$MAX_BUILD_HEIGHT || $y < 0) continue;
 				for($z = 0; $z < count(array_keys($this->copy[$player->getName()][$x][$y])); $z++){
 					if(!$level->isChunkLoaded($x, $z)) $level->loadChunk($x, $z, true);
-					$this->undo[$player->getName()][$x][$y][$z] = $level->getBlock($pos->add($x, $y, $z));
+					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock(new Vector3($x, $y, $z)));
 					$level->setBlockIdAt($pos->x + $x, $pos->y + $y, $pos->z + $z, $this->copy[$player->getName()][$x][$y][$z]->getId());
 					$level->setBlockDataAt($pos->x + $x, $pos->y + $y, $pos->z + $z, $this->copy[$player->getName()][$x][$y][$z]->getDamage());
 				}
@@ -295,40 +356,45 @@ class Main extends PluginBase implements Listener{
 
 	public function undo(Player $player){
 		$level = $player->getLevel();
-		$pos = $this->undopos[$player->getName()];
-		for($x = 0; $x < count(array_keys($this->undo[$player->getName()])); $x++){
-			for($y = 0; $y < count(array_keys($this->undo[$player->getName()][$x])); $y++){
-				for($z = 0; $z < count(array_keys($this->undo[$player->getName()][$x][$y])); $z++){
-					$level->setBlockIdAt($pos->x + $x, $pos->y + $y, $pos->z + $z, $this->undo[$player->getName()][$x][$y][$z]->getId());
-					$level->setBlockDataAt($pos->x + $x, $pos->y + $y, $pos->z + $z, $this->undo[$player->getName()][$x][$y][$z]->getDamage());
-				}
-			}
+		if(!isset($this->undo[$player->getName()])) return;
+		$undo = array_pop($this->undo[$player->getName()]);
+		foreach($undo as $block){
+			$level->setBlockIdAt($block->x, $block->y, $block->z, $block->getId());
+			$level->setBlockDataAt($block->x, $block->y, $block->z, $block->getDamage());
 		}
-		$this->undopos[$player->getName()] = null;
+		$this->redo[$player->getName()][] = $undo;
 		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Undo succeed.");
 	}
 
-	public function wandPos1(BlockBreakEvent $event){
-		$sender = $event->getPlayer();
-		$block = $event->getBlock();
-		if($sender->hasPermission("magicwe.command.wand") && $sender->getInventory()->getItemInHand()->getId() === Item::WOODEN_AXE && $this->wand[$sender->getName()] === 1){
-			if($block->y > self::$MAX_BUILD_HEIGHT || $block->y < 0) $sender->sendMessage(TextFormat::GOLD . "[MagicWE] Warning: You are above y:" . self::$MAX_BUILD_HEIGHT . " or below y:0");
-			$this->pos1[$sender->getName()] = $block;
-			$sender->sendMessage(TextFormat::GREEN . "[MagicWE] Position 1 set as x:" . $block->x . " y:" . $block->y . " z:" . $block->z);
-			$event->setCancelled();
+	public function redo(Player $player){
+		$level = $player->getLevel();
+		if(!isset($this->redo[$player->getName()])) return;
+		$redo = array_pop($this->redo[$player->getName()]);
+		foreach($redo as $block){
+			$level->setBlockIdAt($block->x, $block->y, $block->z, $block->getId());
+			$level->setBlockDataAt($block->x, $block->y, $block->z, $block->getDamage());
 		}
+		$this->undo[$player->getName()] = $redo;
+		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Redo succeed.");
 	}
 
-	public function wandPos2(PlayerInteractEvent $event){
-		if($event->getAction() !== PlayerInteractEvent::RIGHT_CLICK_BLOCK) return;
-		$sender = $event->getPlayer();
-		$block = $event->getBlock();
-		if($sender->hasPermission("magicwe.command.wand") && $sender->getInventory()->getItemInHand()->getId() === Item::WOODEN_AXE && $this->wand[$sender->getName()] === 1){
-			if($block->y > self::$MAX_BUILD_HEIGHT || $block->y < 0) $sender->sendMessage(TextFormat::GOLD . "[MagicWE] Warning: You are above y:" . self::$MAX_BUILD_HEIGHT . " or below y:0");
-			$this->pos2[$sender->getName()] = $block;
-			$sender->sendMessage(TextFormat::GREEN . "[MagicWE] Position 2 set as x:" . $block->x . " y:" . $block->y . " z:" . $block->z);
-			$event->setCancelled();
+	public function flip(Player $player, $xyz){
+		if($xyz === "x"){
+			$this->copy[$player->getName()] = array_reverse($this->copy[$player->getName()]);
 		}
+		elseif($xyz === "y"){
+			foreach($this->copy[$player->getName()] as $block){
+				$this->copy[$player->getName()][$block->x] = array_reverse($this->copy[$player->getName()][$block->x]);
+			}
+		}
+		elseif($xyz === "z"){
+			foreach($this->copy[$player->getName()] as $block){
+				foreach($block->x as $y){
+					$this->copy[$player->getName()][$block->x][$y] = array_reverse($this->copy[$player->getName()][$block->x][$y]);
+				}
+			}
+		}
+		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Clipboard flipped on $xyz-Axis");
 	}
 	
 	// structures
@@ -444,13 +510,18 @@ class Main extends PluginBase implements Listener{
 	
 	// schematic
 	// TODO
-	public function pasteSchematic(Level $level, Position $loc, SchematicLoader $schematic){
+	public function pasteSchematic(Player $player, Level $level, Position $loc, SchematicLoader $schematic){
 		$blocks = $schematic->getBlocksArray();
+		if(!isset($this->undo[$player->getName()])) $this->undo[$player->getName()] = [];
+		$undoindex = count(array_keys($this->undo[$player->getName()]));
+		$this->undo[$player->getName()][$undoindex] = [];
 		foreach($blocks as $block){
 			if($block[1] > self::$MAX_BUILD_HEIGHT) continue;
 			if(!$level->isChunkLoaded($block[0], $block[2])) $level->loadChunk($block[0], $block[2], true);
-			$level->setBlockIdAt($block[0] + $loc->getX(), $block[1] + $loc->getY(), $block[2] + $loc->getZ(), $block[3]);
-			$level->setBlockDataAt($block[0] + $loc->getX(), $block[1] + $loc->getY(), $block[2] + $loc->getZ(), $block[4]);
+			$blockloc = $loc->add($block[0], $block[1], $block[2]);
+			array_push($this->undo[$player->getName()][$undoindex], $level->getBlock($blockloc));
+			$level->setBlockIdAt($blockloc->getX(), $blockloc->getY(), $blockloc->getZ(), $block[3]);
+			$level->setBlockDataAt($blockloc->getX(), $blockloc->getY(), $blockloc->getZ(), $block[4]);
 		}
 		return true;
 	}
@@ -465,17 +536,60 @@ class Main extends PluginBase implements Listener{
 		$data = '';
 		$pos1 = $this->pos1[$sender->getName()];
 		$pos2 = $this->pos2[$sender->getName()];
-		$w = max($pos1->x, $pos2->x) - min($pos1->x, $pos2->x) + 1;
-		$h = max($pos1->y, $pos2->y) - min($pos1->y, $pos2->y) + 1;
-		$l = max($pos1->z, $pos2->z) - min($pos1->z, $pos2->z) + 1;
-		for($x = 0; $x <= $w; $x++){
-			for($y = 0; $y <= $h && $y <= self::$MAX_BUILD_HEIGHT; $y++){
-				for($z = 0; $z <= $l; $z++){
-					$block = $sender->getLevel()->getBlock($pos1->add($x, $y, $z));
-					$id = chr($block->getId());
-					$damage = chr($block->getDamage());
-					$blocks .= $id;
-					$data .= $damage;
+		$origin = new Vector3(min($pos1->x, $pos2->x), min($pos1->y, $pos2->y), min($pos1->z, $pos2->z));
+		$w = abs($pos1->x - $pos2->x) + 1;
+		$h = abs($pos1->y - $pos2->y) + 1;
+		$l = abs($pos1->z - $pos2->z) + 1;
+		$blocks = '';
+		$data = '';
+		for($y = 0; $y < $h; $y++){
+			for($z = 0; $z < $l; $z++){
+				for($x = 0; $x < $w; $x++){
+					$block = $sender->getLevel()->getBlock($origin->add($x, $y, $z));
+					$id = $block->getId();
+					$damage = $block->getDamage();
+					switch($id){
+						case 158:
+							$id = 126;
+							break;
+						case 157:
+							$id = 125;
+							break;
+						case 126:
+							$id = 157;
+							break;
+						case 85:
+							switch($damage){
+								case 1:
+									$id = 188;
+									$damage = 0;
+									break;
+								case 2:
+									$id = 189;
+									$damage = 0;
+									break;
+								case 3:
+									$id = 190;
+									$damage = 0;
+									break;
+								case 4:
+									$id = 191;
+									$damage = 0;
+									break;
+								case 5:
+									$id = 192;
+									$damage = 0;
+									break;
+								default:
+									$damage = 0;
+									break;
+							}
+							break;
+						default:
+							break;
+					}
+					$blocks .= chr($id);
+					$data .= chr($damage);
 				}
 			}
 		}
