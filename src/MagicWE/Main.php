c558490110b326d8ada601a24bf1101f83d90566
thebigsmileXD <thebigsmilexd@gmail.com>
2016-06-27 23:38:51 +0200

    Block names, time + block counter, automatic gc
    -automatic garbage cleanup
    -support for using blocknames instead of id's
    -estimated time + block changed counter
    -fixed secret bug
    -fixed //set air not working
    -added "thank you all users with ideas" in plugin header
    -changed version to 0.2.1

--- src/MagicWE/Main.php
+++ src/MagicWE/Main.php
@@ -10,6 +10,8 @@
  * Thanks so much to @svilex for creating most of the schematics import + export code! (https://github.com/svilex/Schematic_Loader)
  * You are so awesome, dude! Couldn't have got this done without you!
  *
+ * Thanks for all suggestions from you all!
+ *
  * https://github.com/thebigsmileXD
  * https://github.com/svilex
  */
@@ -78,6 +80,7 @@ class Main extends PluginBase implements Listener{
 						if(isset($args[0])){
 							if(isset($this->pos1[$sender->getName()], $this->pos2[$sender->getName()])){
 								$this->fill($sender, $args[0]);
+								$sender->getLevel()->doChunkGarbageCollection();
 								return true;
 							}
 						}
@@ -93,6 +96,7 @@ class Main extends PluginBase implements Listener{
 						if(isset($args[0]) && isset($args[1])){
 							if(isset($this->pos1[$sender->getName()], $this->pos2[$sender->getName()])){
 								$this->replace($sender, $args[0], $args[1]);
+								$sender->getLevel()->doChunkGarbageCollection();
 								return true;
 							}
 						}
@@ -117,6 +121,7 @@ class Main extends PluginBase implements Listener{
 						if(!$sender->hasPermission("magicwe.command.paste")) return;
 						if(isset($this->pos1[$sender->getName()], $this->pos2[$sender->getName()])){
 							$this->paste($sender);
+							$sender->getLevel()->doChunkGarbageCollection();
 							return true;
 						}
 						break;
@@ -127,6 +132,7 @@ class Main extends PluginBase implements Listener{
 						if(!$sender->hasPermission("magicwe.command.undo")) return;
 						if(!empty($this->undo[$sender->getName()])){
 							$this->undo($sender);
+							$sender->getLevel()->doChunkGarbageCollection();
 							return true;
 						}
 						else{
@@ -140,6 +146,7 @@ class Main extends PluginBase implements Listener{
 						if(!$sender->hasPermission("magicwe.command.redo")) return;
 						if(!empty($this->redo[$sender->getName()])){
 							$this->redo($sender);
+							$sender->getLevel()->doChunkGarbageCollection();
 							return true;
 						}
 						else{
@@ -202,6 +209,7 @@ class Main extends PluginBase implements Listener{
 								$success = $this->pasteSchematic($sender, $sender->getLevel(), $sender->getPosition(), $this->schematics[$args[1]]);
 								if($success){
 									$sender->sendMessage(TextFormat::GREEN . "[MagicWE] Schematic $args[1] sucessfully pasted");
+									$sender->getLevel()->doChunkGarbageCollection();
 									return true;
 								}
 							}
@@ -257,6 +265,8 @@ class Main extends PluginBase implements Listener{
 	}
 
 	public function fill(Player $player, $blockarg){
+		$changed = 0;
+		$time = microtime(TRUE);
 		if(empty($blockarg) && $blockarg !== "0") return false;
 		$level = $player->getLevel();
 		$blocks = explode(",", $blockarg);
@@ -270,20 +280,27 @@ class Main extends PluginBase implements Listener{
 			for($y = $pos->y; $y <= max($pos1->y, $pos2->y); $y++){
 				if($y > self::$MAX_BUILD_HEIGHT || $y < 0) continue;
 				for($z = $pos->z; $z <= max($pos1->z, $pos2->z); $z++){
-					if(!$level->isChunkLoaded($x, $z)) $level->loadChunk($x, $z, true);
-					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock(new Vector3($x, $y, $z)));
+					if(!$level->isChunkLoaded($x >> 4, $z >> 4)) $level->loadChunk($x >> 4, $z >> 4, true);
+					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock($vec = new Vector3($x, $y, $z)));
 					$blockstring = $blocks[array_rand($blocks, 1)];
-					$block = explode(":", $blockstring)[0];
-					$level->setBlockIdAt($x, $y, $z, $block);
-					$meta = (isset(explode(":", $blockstring)[1])?explode(":", $blockstring)[1]:0);
-					$level->setBlockDataAt($x, $y, $z, $meta);
+					$block = Item::fromString($blockstring)->getBlock();
+					if($block->getId() === 0 && !(strtolower(explode(":", $blockstring)[0]) == "air" || explode(":", $blockstring)[0] == "0")){
+						$player->sendMessage(TextFormat::RED . '[MagicWE] No such block/item found: "' . $blockstring . '", aborting');
+						$player->sendMessage(TextFormat::RED . "[MagicWE] Fill failed.");
+						return;
+					}
+					// $level->setBlockIdAt($x, $y, $z, $block->getId());
+					// $level->setBlockDataAt($x, $y, $z, $block->getDamage());
+					if($level->setBlock($vec, $block, false, false)) $changed++;
 				}
 			}
 		}
-		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Fill succeed.");
+		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Fill succeed, took " . round((microtime(TRUE) - $time), 2) . "s, " . $changed . " Blocks changed.");
 	}
 
 	public function replace(Player $player, $blockarg1, $blockarg2){
+		$changed = 0;
+		$time = microtime(TRUE);
 		if((empty($blockarg1) && $blockarg1 !== "0") || (empty($blockarg2) && $blockarg2 !== "0")) return false;
 		$level = $player->getLevel();
 		$blocks1 = explode(",", $blockarg1);
@@ -298,23 +315,36 @@ class Main extends PluginBase implements Listener{
 			for($y = $pos->y; $y <= max($pos1->y, $pos2->y); $y++){
 				if($y > self::$MAX_BUILD_HEIGHT || $y < 0) continue;
 				for($z = $pos->z; $z <= max($pos1->z, $pos2->z); $z++){
-					if(!$level->isChunkLoaded($x, $z)) $level->loadChunk($x, $z, true);
-					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock(new Vector3($x, $y, $z)));
+					if(!$level->isChunkLoaded($x >> 4, $z >> 4)) $level->loadChunk($x >> 4, $z >> 4, true);
+					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock($vec = new Vector3($x, $y, $z)));
 					foreach($blocks1 as $blockstring1){
-						$block1 = explode(":", $blockstring1)[0];
-						$meta1 = (isset(explode(":", $blockstring1)[1])?explode(":", $blockstring1)[1]:false);
+						$blocka = Item::fromString($blockstring1)->getBlock();
+						if($blocka->getId() === 0 && !(strtolower(explode(":", $blockstring1)[0]) == "air" || explode(":", $blockstring1)[0] == "0")){
+							$player->sendMessage(TextFormat::RED . '[MagicWE] No such block/item found: "' . $blockstring1 . '", aborting');
+							$player->sendMessage(TextFormat::RED . "[MagicWE] Replace failed.");
+							return;
+						}
+						$block1 = $blocka->getId();
+						$meta1 = (explode(":", $blockstring1)[1]??false);
 						if($level->getBlockIdAt($x, $y, $z) == $block1 && ($meta1 === false || $level->getBlockDataAt($x, $y, $z) == $meta1)){
 							$blockstring2 = $blocks2[array_rand($blocks2, 1)];
-							$block2 = explode(":", $blockstring2)[0];
-							$level->setBlockIdAt($x, $y, $z, $block2);
-							$meta2 = (isset(explode(":", $blockstring2)[1])?explode(":", $blockstring2)[1]:0);
-							$level->setBlockDataAt($x, $y, $z, $meta2);
+							$blockb = Item::fromString($blockstring2)->getBlock();
+							if($blockb->getId() === 0 && !(strtolower(explode(":", $blockstring2)[0]) == "air" || explode(":", $blockstring2)[0] == "0")){
+								$player->sendMessage(TextFormat::RED . '[MagicWE] No such block/item found: "' . $blockstring2 . '", aborting');
+								$player->sendMessage(TextFormat::RED . "[MagicWE] Replace failed.");
+								return;
+							}
+							// $block2 = $blockb->getId();
+							if($level->setBlock($vec, $blockb, false, false)) $changed++;
+							// $level->setBlockIdAt($x, $y, $z, $block2);
+							// $meta2 = (explode(":", $blockstring2)[1]??0);
+							// $level->setBlockDataAt($x, $y, $z, $meta2);
 						}
 					}
 				}
 			}
 		}
-		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Replace succeed.");
+		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Replace succeed, took " . round((microtime(TRUE) - $time), 2) . "s, " . $changed . " Blocks changed.");
 	}
 
 	public function copy(Player $player){
@@ -336,6 +366,7 @@ class Main extends PluginBase implements Listener{
 	}
 
 	public function paste(Player $player){
+		$time = microtime(TRUE);
 		$level = $player->getLevel();
 		$pos = $player->getPosition()->add($this->copypos[$player->getName()]);
 		if(!isset($this->undo[$player->getName()])) $this->undo[$player->getName()] = [];
@@ -345,17 +376,18 @@ class Main extends PluginBase implements Listener{
 			for($y = 0; $y < count(array_keys($this->copy[$player->getName()][$x])); $y++){
 				if($y > self::$MAX_BUILD_HEIGHT || $y < 0) continue;
 				for($z = 0; $z < count(array_keys($this->copy[$player->getName()][$x][$y])); $z++){
-					if(!$level->isChunkLoaded($x, $z)) $level->loadChunk($x, $z, true);
+					if(!$level->isChunkLoaded($x >> 4, $z >> 4)) $level->loadChunk($x >> 4, $z >> 4, true);
 					array_push($this->undo[$player->getName()][$undoindex], $level->getBlock(new Vector3($x, $y, $z)));
 					$level->setBlockIdAt($pos->x + $x, $pos->y + $y, $pos->z + $z, $this->copy[$player->getName()][$x][$y][$z]->getId());
 					$level->setBlockDataAt($pos->x + $x, $pos->y + $y, $pos->z + $z, $this->copy[$player->getName()][$x][$y][$z]->getDamage());
 				}
 			}
 		}
-		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Pasting succeed.");
+		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Pasting succeed, took " . round((microtime(TRUE) - $time), 2) . "s");
 	}
 
 	public function undo(Player $player){
+		$time = microtime(TRUE);
 		$level = $player->getLevel();
 		if(!isset($this->undo[$player->getName()])) return;
 		$undo = array_pop($this->undo[$player->getName()]);
@@ -364,10 +396,11 @@ class Main extends PluginBase implements Listener{
 			$level->setBlockDataAt($block->x, $block->y, $block->z, $block->getDamage());
 		}
 		$this->redo[$player->getName()][] = $undo;
-		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Undo succeed.");
+		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Undo succeed, took " . round((microtime(TRUE) - $time), 2) . "s");
 	}
 
 	public function redo(Player $player){
+		$time = microtime(TRUE);
 		$level = $player->getLevel();
 		if(!isset($this->redo[$player->getName()])) return;
 		$redo = array_pop($this->redo[$player->getName()]);
@@ -376,7 +409,7 @@ class Main extends PluginBase implements Listener{
 			$level->setBlockDataAt($block->x, $block->y, $block->z, $block->getDamage());
 		}
 		$this->undo[$player->getName()] = $redo;
-		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Redo succeed.");
+		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Redo succeed, took " . round((microtime(TRUE) - $time), 2) . "s");
 	}
 
 	public function flip(Player $player, $xyz){
@@ -395,7 +428,8 @@ class Main extends PluginBase implements Listener{
 				}
 			}
 		}
-		else return false;
+		else
+			return false;
 		$player->sendMessage(TextFormat::GREEN . "[MagicWE] Clipboard flipped on $xyz-Axis");
 	}
 	
@@ -519,7 +553,7 @@ class Main extends PluginBase implements Listener{
 		$this->undo[$player->getName()][$undoindex] = [];
 		foreach($blocks as $block){
 			if($block[1] > self::$MAX_BUILD_HEIGHT) continue;
-			if(!$level->isChunkLoaded($block[0], $block[2])) $level->loadChunk($block[0], $block[2], true);
+			if(!$level->isChunkLoaded($block[0] >> 4, $block[2] >> 4)) $level->loadChunk($block[0] >> 4, $block[2] >> 4, true);
 			$blockloc = $loc->add($block[0], $block[1], $block[2]);
 			array_push($this->undo[$player->getName()][$undoindex], $level->getBlock($blockloc));
 			$level->setBlockIdAt($blockloc->getX(), $blockloc->getY(), $blockloc->getZ(), $block[3]);
